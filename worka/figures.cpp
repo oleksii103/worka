b399fb7ce25f27188643e5619ec417504e1fb89c
oleksii103 <132712296+oleksii103@users.noreply.github.com>
2024-11-16 12:47:37 +0200

    figures

--- worka/figures.cpp
+++ worka/figures.cpp
@@ -2,21 +2,19 @@
 #include <cmath>
 #include <string>
 #include <fstream>
+#include <sstream>
 
-//figure code copy-paste from 02.11.2024
 using namespace std;
 
 const float Pi = 3.141592653589793;
 
 class Figures {
 public:
-    friend ostream& operator<<(ostream& out, Figures& figure) {
-        out << "I am a figure. ";
-        return out;
-    }
     virtual float Area() = 0;
     virtual float Perimeter() = 0;
     virtual ~Figures() {}
+    virtual void PrintToFile(ofstream& outfile) = 0;
+    virtual void PrintFromFile(ifstream& infile) = 0;
 };
 
 class Parallelogram : public Figures {
@@ -30,12 +28,21 @@ public:
         float angleRadians = Angle * Pi / 180.0;
         return SideA * SideB * sin(angleRadians);
     }
+
     float Perimeter() override {
         return 2 * (SideA + SideB);
     }
-    friend ostream& operator<<(ostream& out, Parallelogram& para) {
-        out << "Parallelogram: " << para.SideA << ", " << para.SideB << ", " << para.Angle << " degrees.";
-        return out;
+
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Parallelogram: " << SideA << ", " << SideB << ", " << Angle << " degrees." << endl;
+        outfile << "Area: " << Area() << ", Perimeter: " << Perimeter() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
@@ -44,9 +51,16 @@ public:
     Rectangle() : Parallelogram(1, 2, 90) {}
     Rectangle(int sideA, int sideB) : Parallelogram(sideA, sideB, 90) {}
 
-    friend ostream& operator<<(ostream& out, Rectangle& rect) {
-        out << "Rectangle: " << rect.SideA << " x " << rect.SideB;
-        return out;
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Rectangle: " << SideA << " x " << SideB << endl;
+        outfile << "Area: " << Area() << ", Perimeter: " << Perimeter() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
@@ -60,12 +74,21 @@ public:
     float Area() override {
         return Pi * R * R;
     }
+
     float Perimeter() override {
         return 2 * Pi * R;
     }
-    friend ostream& operator<<(ostream& out, Circle& circle) {
-        out << "Circle with radius: " << circle.R;
-        return out;
+
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Circle with radius: " << R << endl;
+        outfile << "Area: " << Area() << ", Perimeter: " << Perimeter() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
@@ -74,6 +97,8 @@ public:
     virtual double Volume() = 0;
     virtual double FullArea() = 0;
     virtual ~D3Figure() {}
+    virtual void PrintToFile(ofstream& outfile) = 0;
+    virtual void PrintFromFile(ifstream& infile) = 0;
 };
 
 class Cube : public D3Figure {
@@ -91,9 +116,16 @@ public:
         return 6 * Side * Side;
     }
 
-    friend ostream& operator<<(ostream& out, Cube& cube) {
-        out << "Cube with side: " << cube.Side;
-        return out;
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Cube with side: " << Side << endl;
+        outfile << "Volume: " << Volume() << ", Full Area: " << FullArea() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
@@ -112,9 +144,16 @@ public:
         return 4 * Pi * R * R;
     }
 
-    friend ostream& operator<<(ostream& out, Sphere& sphere) {
-        out << "Sphere with radius: " << sphere.R;
-        return out;
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Sphere with radius: " << R << endl;
+        outfile << "Volume: " << Volume() << ", Full Area: " << FullArea() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
@@ -134,72 +173,139 @@ public:
         return SideA + SideB + hypotenuse;
     }
 
-    friend ostream& operator<<(ostream& out, RightTriangle& tri) {
-        out << "Right Triangle: sides = " << tri.SideA << ", " << tri.SideB;
-        return out;
+    void PrintToFile(ofstream& outfile) override {
+        outfile << "Right Triangle: sides = " << SideA << ", " << SideB << endl;
+        outfile << "Area: " << Area() << ", Perimeter: " << Perimeter() << endl;
+    }
+
+    void PrintFromFile(ifstream& infile) override {
+        string line;
+        while (getline(infile, line)) {
+            cout << line << endl;
+        }
     }
 };
 
+void DisplayFileContent(const string& filename) {
+    ifstream infile(filename);
+    if (!infile.is_open()) {
+        cerr << "Failed to open file for reading!" << endl;
+        return;
+    }
+
+    string line;
+    while (getline(infile, line)) {
+        cout << line << endl;
+    }
+
+    infile.close();
+}
+
 int main() {
     string filename = "figures_output.txt";
-
-    //user interface
     int userChoice;
-    cout << "Choose an option:\n";
-    cout << "1 - Write to file (append)\n";
-    cout << "2 - Clear the file before writing\n";
-    cin >> userChoice;
+    string inputLine;
 
-    //chat gpt work whis file "add to file" "clear file" (it`s doesn`t work) 
-    ofstream outfile;
-    if (userChoice == '1') {
-        outfile.open(filename, ios::app); 
+    while (true) { // Loop for repeated actions
+        cout << "\nChoose an option:\n";
+        cout << "1 - Write to file (append)\n";
+        cout << "2 - Clear the file before writing\n";
+        cout << "3 - Display content of the file\n";
+        cout << "4 - Exit\n";
+        cin >> userChoice;
+
+        if (userChoice == 4) {
+            cout << "Exiting the program...\n";
+            break; // Exit the loop and terminate the program
+        }
+
+        ofstream outfile;
+        if (userChoice == 1) {
+            outfile.open(filename, ios::app); // Append to the end of the file
+        }
+        else if (userChoice == 2) {
+            outfile.open(filename, ios::trunc); // Clear the file before writing
+
+            if (!outfile.is_open()) {
+                cerr << "Failed to open file for truncation!" << endl;
+                continue; // Continue the loop if the file can't be opened
+            }
+        }
+        else if (userChoice == 3) {
+            // Display the file content
+            DisplayFileContent(filename);
+            continue; // Return to the start of the loop after displaying file content
+        }
+        else {
+            cerr << "Invalid option selected!" << endl;
+            continue; // Return to the start of the loop if an invalid choice is made
+        }
+
+        if (!outfile.is_open()) {
+            cerr << "Failed to open file for writing!" << endl;
+            continue; // Return to the start of the loop if file can't be opened for writing
+        }
+
+        // Ask the user which objects to write to the file
+        cout << "Choose the objects to write to the file (separate choices with spaces):\n";
+        cout << "1 - Parallelogram\n";
+        cout << "2 - Rectangle\n";
+        cout << "3 - Circle\n";
+        cout << "4 - Right Triangle\n";
+        cout << "5 - Cube\n";
+        cout << "6 - Sphere\n";
+        cout << "7 - Write all objects\n";
+        cout << "Enter your choices (separate multiple choices with space): ";
+        cin.ignore(); // To ignore the leftover newline from previous input
+        getline(cin, inputLine); // Read the full input line
+        stringstream ss(inputLine);
+        int objectChoice;
+
+        // Write objects based on the user's selection
+        while (ss >> objectChoice) {
+            if (objectChoice == 1) {
+                Parallelogram para(3, 5, 60);
+                para.PrintToFile(outfile);
+            }
+            else if (objectChoice == 2) {
+                Rectangle rect(4, 6);
+                rect.PrintToFile(outfile);
+            }
+            else if (objectChoice == 3) {
+                Circle circle(5);
+                circle.PrintToFile(outfile);
+            }
+            else if (objectChoice == 4) {
+                RightTriangle rTri(3, 4);
+                rTri.PrintToFile(outfile);
+            }
+            else if (objectChoice == 5) {
+                Cube cube(5);
+                cube.PrintToFile(outfile);
+            }
+            else if (objectChoice == 6) {
+                Sphere sphere(3);
+                sphere.PrintToFile(outfile);
+            }
+            else if (objectChoice == 7) {
+                // Write all objects to the file
+                Parallelogram para(3, 5, 60);
+                Rectangle rect(4, 6);
+                Circle circle(5);
+                RightTriangle rTri(3, 4);
+                Cube cube(5);
+                Sphere sphere(3);
+                para.PrintToFile(outfile);
+                rect.PrintToFile(outfile);
+                circle.PrintToFile(outfile);
+                rTri.PrintToFile(outfile);
+                cube.PrintToFile(outfile);
+                sphere.PrintToFile(outfile);
+            }
+        }
+
+        outfile.close();
     }
-    else if (userChoice == '2') {
-        outfile.open(filename, ios::trunc); 
-    }
-
-    //wtf are you want you can use only 1 or 2 
-    else {
-        cerr << "Invalid option selected!" << endl;
-        return 1;
-    }
-
-    //can`t open 
-    if (!outfile.is_open()) {
-        cerr << "Failed to open file for writing!" << endl;
-        return 1;
-    }
-
-    //create objects
-    Parallelogram para(3, 5, 60);
-    Rectangle rect(4, 6);
-    Circle circle(5);
-    RightTriangle rTri(3, 4);
-    Cube cube(5);
-    Sphere sphere(3);
-
-    //data output
-    outfile << para << endl;
-    outfile << "Area: " << para.Area() << ", Perimeter: " << para.Perimeter() << endl;
-
-    outfile << rect << endl;
-
-    outfile << circle << endl;
-    outfile << "Area: " << circle.Area() << ", Perimeter: " << circle.Perimeter() << endl;
-
-    outfile << rTri << endl;
-
-    outfile << cube << endl;
-    outfile << "Volume: " << cube.Volume() << ", Full Area: " << cube.FullArea() << endl;
-
-    outfile << sphere << endl;
-    outfile << "Volume: " << sphere.Volume() << ", Full Area: " << sphere.FullArea() << endl;
-
-    outfile.close();
-
-    //success 
-    cout << "Data successfully written to file \"" << filename << "\"." << endl;
 
     return 0;
 }
